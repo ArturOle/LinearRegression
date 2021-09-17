a022925c29e0c722efa4da8f6da9bc47cd4831d4
ArturOle <artur.oleksinski99@gmail.com>
2021-09-17 15:24:07 +0200

    Adding date handling - wip

--- LinReg.jl
+++ LinReg.jl
@@ -1,4 +1,5 @@
 using Base: Float64, Int64
+using Dates
 using Plots
 
 pow_2(x::Any) = x*x 
@@ -8,7 +9,7 @@ pow_3(x::Any) = x*x*x
 # Linear regression for one variable functions
 function linreg(funk::Function, x_range::Vector{T} where T, plot::Bool=false) 
     # Vectors
-    data = []
+    y = []
     x    = []
 
     # Sums
@@ -25,22 +26,22 @@ function linreg(funk::Function, x_range::Vector{T} where T, plot::Bool=false)
     mean_x = 0
     mean_y = 0
 
-    # Generating the data with the function
+    # Generating the(y with the function
     for i in x_range[1]:x_range[2]:x_range[3]
         y = funk(i)
         sum_x += i
         sum_y += y
         append!(x, i)
-        append!(data, y)
+        append!(y, y)
     end
 
     # Calculating means
-    mean_y = sum_y/length(data)
-    mean_x = sum_x/length(data)
+    mean_y = sum_y/length(y)
+    mean_x = sum_x/length(y)
 
     # Calculating the sums of numerator and denumerator seperatly
     for i in 1:length(x)
-        beta_top += ((x[i]-mean_x)*(data[i]-mean_y))
+        beta_top += ((x[i]-mean_x)*(y[i]-mean_y))
         beta_bot += ((x[i]-mean_x)^2)
     end
 
@@ -54,7 +55,7 @@ function linreg(funk::Function, x_range::Vector{T} where T, plot::Bool=false)
         println("mean of x = ", mean_x, "    mean of y = ", mean_y)
 
         # Ploting the function and the linear regression line
-        scatter(x, data)
+        scatter(x, y)
         plot!([x[1], x[end]], [beta*x[1]+alfa, beta*x[end]+alfa])
     else
         return alfa, beta
@@ -66,7 +67,7 @@ function linreg(x_range::Vector{T} where T, y_range::Vector{T} where T, plot::Bo
     if length(x_range) == length(y_range)
 
         # Vectors
-        data = y_range
+        y = y_range
         x    = x_range
 
         # Sums
@@ -84,12 +85,12 @@ function linreg(x_range::Vector{T} where T, y_range::Vector{T} where T, plot::Bo
         mean_y = 0
 
         # Calculating means
-        mean_y = sum(y_range)/length(data)
-        mean_x = sum(x_range)/length(data)
+        mean_y = sum(y_range)/length(y)
+        mean_x = sum(x_range)/length(y)
 
         # Calculating the sums of numerator and denumerator seperatly
         for i in 1:length(x)
-            beta_top += ((x[i]-mean_x)*(data[i]-mean_y))
+            beta_top += ((x[i]-mean_x)*(y[i]-mean_y))
             beta_bot += ((x[i]-mean_x)^2)
         end
 
@@ -103,7 +104,7 @@ function linreg(x_range::Vector{T} where T, y_range::Vector{T} where T, plot::Bo
             println("mean of x = ", mean_x, "    mean of y = ", mean_y)
 
             # Ploting the function and the linear regression line
-            scatter(x, data)
+            scatter(x, y)
             plot!([x[1], x[end]], [beta*x[1]+alfa, beta*x[end]+alfa])
         else
             return alfa, beta
@@ -113,4 +114,82 @@ function linreg(x_range::Vector{T} where T, y_range::Vector{T} where T, plot::Bo
     end
 end
 
-linreg([1, 3, 2, 5, 4, 6], [0.1, 0.2, 1.1, 0.5, 0.6, 0.8], true)
+function linreg(only_dates::Vector{Date}, y_range::Vector{T} where T, plot::Bool)
+
+    # Vectors
+    y = y_range
+    x, dates = map_dates(only_dates)
+    # dates = [ Dates.format(z, "yyyy-mm-dd") for z in dates ]
+    println(x)
+
+    # Sums
+    sum_x::Float64 = 0
+    sum_y::Float64 = 0
+    
+    # Coefficients
+    beta_top = 0
+    beta_bot = 0
+    beta     = 0
+    alfa     = 0
+
+    # Means
+    mean_x = 0
+    mean_y = 0
+
+    # Calculating means
+    mean_y = sum(y)/length(y)
+    mean_x = sum(x)/length(y)
+
+    # Calculating the sums of numerator and denumerator seperatly
+    for i in 1:length(x)
+        beta_top += ((x[i]-mean_x)*(y[i]-mean_y))
+        beta_bot += ((x[i]-mean_x)^2)
+    end
+
+    # Calculating coefficients
+    beta = beta_top/beta_bot
+    alfa = mean_y - mean_x*beta
+
+    if plot
+        # Presentation of the results
+        println("α = " , alfa, "   β = ", beta )
+        println("mean of x = ", mean_x, "    mean of y = ", mean_y)
+
+        # Ploting the function and the linear regression line
+        scatter(x, y, xticks=Dates.value.(dates))
+        plot!([x[1], x[end]], [beta*x[1]+alfa, beta*x[end]+alfa])
+    else
+        return alfa, beta
+    end
+end
+
+
+function map_dates(dates::Vector{Date})
+    mapped_dates = []
+    indexes      = []
+    new_index    = 0
+    for date in dates
+        if date in mapped_dates
+            push!(indexes, findfirst(x->x==date, mapped_dates))
+        else
+            push!(mapped_dates, date)
+            push!(indexes, new_index)
+            new_index += 1
+        end
+    end
+    return indexes, mapped_dates
+end
+
+linreg(
+    [
+        Date(2004,1,20), Date(2004,3,23), Date(2004,1,20), 
+        Date(2004,7,11), Date(2004,7,11), Date(2004,6,10)
+    ], 
+    
+    [
+        0.1, 0.2, 1.1, 
+        0.5, 0.6, 0.8
+    ], 
+    
+    true
+)
